2c25e92cc05cea7b47d89666df60caf46bc08572
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 12:47:35 -0500

    mocha frapp class

--- src/main/java/com/starbuzz/MochaFrappucchino.java
+++ src/main/java/com/starbuzz/MochaFrappucchino.java
@@ -1,5 +1,15 @@
 package main.java.com.starbuzz;
 
-public class MochaFrappucchino {
+public class MochaFrappucchino extends Condiment {
+
+    @Override
+    public double cost() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
 
 }
