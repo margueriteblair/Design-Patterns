d69d6fcee31016da40f3650e6e5d2c87c980893c
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 12:56:22 -0500

    frapp class

---
+++ src/main/java/com/starbuzz/Frappuccino.java
@@ -0,0 +1,13 @@
+package main.java.com.starbuzz;
+
+public class Frappuccino extends Beverage{
+    public Frappuccino() {
+        this.description = "Frappuccino";
+    }
+
+
+    @Override
+    public double cost() {
+        return 0;
+    }
+}
