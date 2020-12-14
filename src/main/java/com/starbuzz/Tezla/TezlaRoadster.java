5ecbefa785cfdf379ce55cbae7da003cfa69cd78
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 13:19:36 -0500

    implementing states with our roadster

--- src/main/java/com/starbuzz/Tezla/TezlaRoadster.java
+++ src/main/java/com/starbuzz/Tezla/TezlaRoadster.java
@@ -1,8 +1,14 @@
 package main.java.com.starbuzz.Tezla;
 
+import java.sql.Driver;
+
 //State pattern
 public class TezlaRoadster {
 
+    State drive = new Drive();
+    State parked = new Parked();
+    State reverse = new Reverse();
+    State currentState = parked;
 
     public void accelerate() {
         System.out.println("Go.");
