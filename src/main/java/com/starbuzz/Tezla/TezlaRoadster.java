fed41332d4cdc97087acec61f3e754c4ef818130
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 13:12:12 -0500

    state design pattern practicing with opening door

--- src/main/java/com/starbuzz/Tezla/TezlaRoadster.java
+++ src/main/java/com/starbuzz/Tezla/TezlaRoadster.java
@@ -1,8 +1,21 @@
 package main.java.com.starbuzz.Tezla;
 
+//State pattern
 public class TezlaRoadster {
 
+    String[] state = {"Parked", "Drive", "Reverse"};
+
     public void accelerate() {
+        System.out.println("Go.");
+    }
+
+    public void openDoor(String state) {
+        if (state == "Parked") {
+            System.out.println("Open door");
+        } else if (state == "Drive") {
+            System.out.println("Cannot open door");
+        }
+
 
     }
 }
