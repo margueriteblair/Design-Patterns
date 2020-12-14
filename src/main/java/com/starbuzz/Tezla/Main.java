4d00cd2fadee7a8ba252db9e16f7334fb714a338
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 13:29:53 -0500

    tezla

--- src/main/java/com/starbuzz/Tezla/Main.java
+++ src/main/java/com/starbuzz/Tezla/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         TezlaRoadster tezla = new TezlaRoadster();
         tezla.accelerate();
+        tezla.openDoor();
+        tezla.shiftToDrive();
+        tezla.shiftToReverse();
+        tezla.accelerate();
+        tezla.openDoor();
+        tezla.shiftToParked();
     }
 }
